d9356fa66333c5af4e2e77a90e2b4ef2a9c71ea9
Liga <ligacepurniece@gmail.com>
2022-10-23 17:44:43 +0300

    Add services

---
+++ ScooterRental.Services/EntityService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Internal;
+using ScooterRental.Core.Models;
+using ScooterRental.Core.Services;
+using ScooterRental.Data;
+
+namespace ScooterRental.Services
+{
+    public class EntityService<T> : DbService, IEntityService<T> where T : Entity
+    {
+        public EntityService(IScooterRentalDbContext context) : base(context)
+        {
+        }
+
+        public void Create(T entity)
+        {
+            Create<T>(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            Delete<T>(entity);
+        }
+
+        public void Update(T entity)
+        {
+            Update<T>(entity);
+        }
+
+        public List<T> GetAll()
+        {
+            return GetAll<T>();
+        }
+
+        public T GetById(string id)
+        {
+            return GetById<T>(id);
+        }
+
+        public IQueryable<T> Query()
+        {
+            return Query<T>();
+        }
+    }
+}
---
+++ ScooterRental.Services/ScooterService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ScooterRental.Core.Models;
+using ScooterRental.Core.Services;
+using ScooterRental.Data;
+
+namespace ScooterRental.Services
+{
+    public class ScooterService : EntityService<Scooter>, IScooterService
+    {
+        public ScooterService(IScooterRentalDbContext context) : base(context)
+        {
+        }
+
+        public bool ScooterExists(string id)
+        {
+            return _context.Scooters.Any(scooter => scooter.Id == id);
+        }
+
+        public List<Scooter> GetAvailableScooters()
+        {
+            return _context.Scooters.Where(scooter => scooter.IsRented == false).ToList();
+        }
+    }
+}
