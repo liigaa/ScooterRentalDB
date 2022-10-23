7aa9cc85f67f09b08b2900736790f6fecd42cc62
Liga <ligacepurniece@gmail.com>
2022-10-23 17:43:34 +0300

    Add DbContext and migration

---
+++ ScooterRental.Data/IScooterRentalDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using ScooterRental.Core.Models;
+
+namespace ScooterRental.Data
+{
+    public interface IScooterRentalDbContext
+    {
+        DbSet<T> Set<T>() where T : class;
+        EntityEntry<T> Entry<T>(T entity) where T : class;
+        DbSet<Scooter> Scooters { get; set; }
+        DbSet<RentedScooter> RentedScooters { get; set; }
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ ScooterRental.Data/Migrations/20221023121409_Init.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScooterRental.Data.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RentedScooters",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    StarTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TotalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PricePerMinute = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RentedScooters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Scooters",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    IsRented = table.Column<bool>(type: "bit", nullable: false),
+                    PricePerMinute = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scooters", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RentedScooters");
+
+            migrationBuilder.DropTable(
+                name: "Scooters");
+        }
+    }
+}
